0ece7e0015a0781d0e0286cd504c43707d70f51a
Bagas Surya <115177570+bagaswizard@users.noreply.github.com>
2025-07-03 00:05:59 +0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -15,37 +15,22 @@ module tb ();
 
   // Wire up the inputs and outputs:
   reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-
-  // Map pins from the tt_um_top module to the testbench signals
-  assign clk = ui_in[0];
-  wire tx = uo_out[0];
-  wire led0_r = uo_out[1];
-  wire led0_g = uo_out[2];
-  wire led0_b = uo_out[3];
-  wire [3:0] led = uo_out[7:4];
+  wire led0_r;
+  wire led0_g;
+  wire led0_b;
+  wire [3:0] led;
+  wire tx;
   wire ds18b20_dq;
 
-  // Handle bidirectional pin for ds18b20_dq
-  assign ds18b20_dq = uio_oe[0] ? uio_out[0] : 1'bz;
-  assign uio_in[0] = ds18b20_dq;
-
-  // Instantiate the tt_um_top module
-  tt_um_top user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+  // Instantiate the top module
+  top user_project (
+      .clk(clk),
+      .led0_r(led0_r),
+      .led0_g(led0_g),
+      .led0_b(led0_b),
+      .led(led),
+      .tx(tx),
+      .ds18b20_dq(ds18b20_dq)
   );
 
 endmodule
