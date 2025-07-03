c5a47c4d80d2bbe9acb03ea19090422a8c2921ab
Bagas Surya <115177570+bagaswizard@users.noreply.github.com>
2025-07-03 11:34:57 +0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -15,22 +15,24 @@ module tb ();
 
   // Wire up the inputs and outputs:
   reg clk;
-  wire led0_r;
-  wire led0_g;
-  wire led0_b;
-  wire [3:0] led;
-  wire tx;
-  wire ds18b20_dq;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
 
-  // Instantiate the top module
-  tt_um_top tt_um_top (
-      .clk(clk),
-      .led0_r(led0_r),
-      .led0_g(led0_g),
-      .led0_b(led0_b),
-      .led(led),
-      .tx(tx),
-      .ds18b20_dq(ds18b20_dq)
+  // Replace tt_um_example with your module name:
+  tt_um_top user_project (
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
   );
 
 endmodule
